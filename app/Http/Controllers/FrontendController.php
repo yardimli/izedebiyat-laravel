fd93ced89c0aaa5c77d16b7aa62e083f6a766e39
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-02-11 00:36:14 +0800

    Update FrontendController.php

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -159,7 +159,7 @@
 					->where('is_published', 1)
 					->where('moderation_flagged', 0)
 					->orderBy('formul_ekim', 'DESC')
-					->limit(350)
+					->limit(600)
 					->get();
 
 				// Remove duplicate user_ids keeping first occurrence
