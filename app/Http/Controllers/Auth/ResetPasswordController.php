4f21f408966a4614db7a820e20654a6b9c7b589f
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-10-02 09:51:16 +0800

    reset password bug

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -13,24 +13,56 @@
 	{
 		use ResetsPasswords;
 
-		// Set the redirectTo property
+		/**
+		 * Where to redirect users after resetting their password.
+		 *
+		 * @var string
+		 */
+		// MODIFIED: This is kept for reference but will be overridden by our custom response logic.
 		protected $redirectTo = '/sahne-arkasi';
 
-
+		/**
+		 * Reset the given user's password.
+		 *
+		 * @param \Illuminate\Contracts\Auth\CanResetPassword $user
+		 * @param string $password
+		 * @return void
+		 */
+		// MODIFIED: Overriding this method to prevent automatic login after a password reset.
 		protected function resetPassword($user, $password)
 		{
 			$user->password = Hash::make($password);
 			$user->setRememberToken(Str::random(60));
 			$user->save();
-
-			// Log the user in.
-			Auth::login($user);
-//			$this->guard()->login($user);
-
-			// Redirect the user to the /sahne-arkasi page with a success message.
-			return redirect('/sahne-arkasi')->with('status', 'Your password has been changed.');
+			// NOTE: The default behavior logs the user in. We have removed that line
+			// to ensure the user is redirected to the login page to sign in again.
 		}
 
+		/**
+		 * Get the response for a successful password reset.
+		 *
+		 * @param \Illuminate\Http\Request $request
+		 * @param string $response
+		 * @return \Illuminate\Http\RedirectResponse
+		 */
+		// ADDED: A new method to override the default redirect behavior.
+		protected function sendResetResponse(Request $request, $response)
+		{
+			// MODIFIED: Redirect to the 'login' route instead of the dashboard.
+			return redirect()->route('login')
+				// MODIFIED: Add a specific, localized success message to the session.
+				->with('status', 'Şifreniz başarıyla değiştirildi. Lütfen yeni şifrenizle giriş yapın.');
+		}
+
+		/**
+		 * Display the password reset view for the given token.
+		 *
+		 * If no token is present, display the link request form.
+		 *
+		 * @param \Illuminate\Http\Request $request
+		 * @param string|null $token
+		 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+		 */
 		public function showResetPasswordForm(Request $request, $token = null)
 		{
 			return view('auth.passwords.reset')->with(['token' => $token, 'email' => $request->email]);
